a01845cc0f54e83cc3284f5f8b443813fe0604bf
Esteban-ALLIER <esteban.allier@gmail.com>
2024-11-21 17:07:41 +0100

    guide d'utilisation rajouter AP terminé

--- ViewModels/MedicamentEditViewModel.cs
+++ ViewModels/MedicamentEditViewModel.cs
@@ -7,9 +7,11 @@ namespace ASPBookProject.ViewModels;
 public class MedicamentEditViewModel
 {
     [Required]
-    public Medicament? Medicament { get; set; }
-    public List<Antecedent>? Antecedents { get; set; }
-    public List<Allergie>? Allergies { get; set; }
+    public Medicament? Medicament { get; set; } // Le médicaments lui même
+    public List<Antecedent>? Antecedents { get; set; } //La liste de tous les antécédents disponibles
+    public List<Allergie>? Allergies { get; set; } //La liste de toutes les allergies disponibles
+
+    // Les Id sélectionnés dans les listes déroulantes
     public List<int> SelectedAntecedentIds { get; set; } = new List<int>();
     public List<int> SelectedAllergieIds { get; set; } = new List<int>();
 }
